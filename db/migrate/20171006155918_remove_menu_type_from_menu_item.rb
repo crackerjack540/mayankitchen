1bdc78042e4a30bfdcbe76d44b75bf8dc1baab4f
crackerjack540 <awheat1989@gmail.com>
2017-10-06 19:58:24 -0400

    resolved migration issue.

--- db/migrate/20171006155918_remove_menu_type_from_menu_item.rb
+++
@@ -1,5 +0,0 @@
-class RemoveMenuTypeFromMenuItem < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :menu_items, :menu_type, :string
-  end
-end
--- db/migrate/20171006174445_drop_table_menu_item.rb
+++
@@ -1,5 +0,0 @@
-class DropTableMenuItem < ActiveRecord::Migration[5.1]
-  def change
-  	drop_table :menu_item
-  end
-end
