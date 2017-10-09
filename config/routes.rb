9dfdbc2f88f414980746aaf7fec737e596159494
crackerjack540 <awheat1989@gmail.com>
2017-10-09 15:29:50 -0400

    readded routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
 
+  resources :menu_items
 
+  get 'main/dinner'
 
   root 'main#apply'
 
+  get 'main/menu'
+
+  get 'main/apply', as: 'apply'
+
+  get 'main/dessert'
+
+  get 'main/brunch'
+
+  get 'main/lunch'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
