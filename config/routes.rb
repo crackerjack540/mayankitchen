84e98d94f349886cbc4a058c1309971076ff487e
crackerjack540 <awheat1989@gmail.com>
2017-10-09 15:28:31 -0400

    moved apply page to root, and removed all other routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :menu_items
 
-  get 'main/dinner'
 
-  root 'main#home'
+  root 'main#apply'
 
-  get 'main/menu'
-
-  get 'main/apply', as: 'apply'
-
-  get 'main/dessert'
-
-  get 'main/brunch'
-
-  get 'main/lunch'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
