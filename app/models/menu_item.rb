318ce647b41b2b76e7b731722364b5a756079489
crackerjack540 <awheat1989@gmail.com>
2017-10-16 17:37:58 -0400

    added table format to dinner, lunch, brunch, and dessert menus. Addded before_action to menu_items to protect from fraudulent submissions.

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,4 +1,6 @@
 class MenuItem < ApplicationRecord
 	validates :title, :price, :type, presence: true
-	has_many :appetizers
+	before_create :authenticate_model!
+	before_update :authenticate_model!
+	before_destroy :authenticate_model!
 end
