218b83ede7a2808a855451a71c42c2cf91b8d154
crackerjack540 <awheat1989@gmail.com>
2017-10-16 23:45:20 -0400

    removed before_actions from menu_items

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,6 +1,3 @@
 class MenuItem < ApplicationRecord
 	validates :title, :price, :type, presence: true
-	before_create :authenticate_model
-	before_update :authenticate_model
-	before_destroy :authenticate_model
 end
